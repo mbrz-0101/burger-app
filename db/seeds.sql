1c2d62096052e8c68721811b6d92d6bb2704ca11
Matt Brzowski <5thhand@gmail.com>
2018-05-13 12:55:42 -0600

    File creation and connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sinlge Bacon Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Triple Bacon Cheeseburger", FALSE);
