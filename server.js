fd5cc3f04977a1db54ec3eff31cfe1b20b391bd2
Matt Brzowski <5thhand@gmail.com>
2018-05-13 20:34:05 -0600

    Fix syntax to get connection working

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// // Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
 
-// app.use(routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(port, function() {
+app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
