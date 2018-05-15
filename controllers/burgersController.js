43021d51bae28870068b12dc6fbc62e44de4f462
Matt Brzowski <5thhand@gmail.com>
2018-05-15 12:32:16 -0600

    finish database, views, and controller functionality

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -11,38 +11,41 @@ router.get("/", function(req, res) {
     var hbsObject = {
       burger: data
     };
-    console.log(hbsObject);
+
     res.render("index", hbsObject);
   });
 });
 
-// router.post("/api/cats", function(req, res) {
-//   cat.create([
-//     "name", "sleepy"
-//   ], [
-//     req.body.name, req.body.sleepy
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
+router.post("/api/burgers", function(req, res) {
 
-// router.put("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    console.log(result);
+    res.json({ id: result.id });
+  });
+});
 
-//   console.log("condition", condition);
+router.put("/api/burgers/:id", function(req, res) {
 
-//   cat.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+  var condition = "id = " + req.params.id + ";";
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 // router.delete("/api/cats/:id", function(req, res) {
 //   var condition = "id = " + req.params.id;
